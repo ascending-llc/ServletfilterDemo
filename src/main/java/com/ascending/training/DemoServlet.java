8061474e35b1fdfa27239ef2acc015a7c3ee02bb
ryo <ryo.hang@gmail.com>
2020-07-08 10:00:41 -0400

    add routing in the demoservlet

--- src/main/java/com/ascending/training/DemoServlet.java
+++ src/main/java/com/ascending/training/DemoServlet.java
@@ -12,25 +12,36 @@ public class DemoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-
-        // Get an output stream to use in sending the output to the client.
-        ServletOutputStream out = resp.getOutputStream();
-        // Put together the HTML code for the output.
-        out.println("<html>");
-        out.println("<head><title>ASCENDING Servlet Demo</title></head>");
-        out.println("<body>");
-        out.println("<h1>Welcome ASCENDING</h1>");
-        out.println("</body></html>");
 
 //        PrintWriter out = resp.getWriter();
 //        out.println("Welcome ASCENDING");
         System.out.println(req.getServletPath());
-//        if(req.getServletPath().equals("/")){
-//            //todo
-//        }else{
-//            //todo
-//        }
+        if(req.getServletPath().equals("/")){
+            resp.setContentType("text/html");
+
+            // Get an output stream to use in sending the output to the client.
+            ServletOutputStream out = resp.getOutputStream();
+            // Put together the HTML code for the output.
+            out.println("<html>");
+            out.println("<head><title>ASCENDING Servlet Demo</title></head>");
+            out.println("<body>");
+            out.println("<h1>Welcome ASCENDING</h1>");
+            out.println("<a href='/another'>another page link</a>");
+            out.println("</body></html>");
+        }else{
+            resp.setContentType("text/html");
+
+            // Get an output stream to use in sending the output to the client.
+            ServletOutputStream out = resp.getOutputStream();
+            // Put together the HTML code for the output.
+            out.println("<html>");
+            out.println("<head><title>ASCENDING Servlet Demo</title></head>");
+            out.println("<body>");
+            out.println("<h1>Another Page</h1>");
+            out.println("<a href='/'>landing page</a>");
+            out.println("</body></html>");
+
+        }
         System.out.println("We are in HelloServlet");
     }
 }
